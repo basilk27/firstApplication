3de7d5e4207f1a1eb10a7e30e9efd9a450babb18
basilk27 <basilkiwanuka@icloud.com>
2018-07-20 08:24:20 -0400

    Initial commit

---
+++ src/main/java/com/mbsystems/firstapplication/User.java
@@ -0,0 +1,29 @@
+package com.mbsystems.firstapplication;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class User implements Serializable {
+
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword( String password ) {
+        this.password = password;
+    }
+}
